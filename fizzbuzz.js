d1e33abfa9e58098c8cd3c61c8bf239968135039
bvasilop <bvasilop@gmail.com>
2017-03-28 19:51:16 -0400

    added comments

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,27 +1,24 @@
-  
-  // Count through numbers one through 20 and figure out if each one is divisible by 3, 5, or both.
+      // First declare for to describe loop
+      // Secondly declare a variable for i 
+      // Then count through numbers one through 20 
   for (var i = 1; i <= 20; i++) {
-  // For each number, do the following check:
-
-  // See if it is divisible by 15
-  
-    if(i % 15 === 0) {
-      // If divisible by 15: Print FizzBuzz (any number divisible by 3 and 5 is also divisible by 15)
-
+      // Use if conditional to figure out if the numbers are divisible by 3, 5, or both.
+      // To simplify and condense code see if it is divisible by 15 --any number divisible by 3 and 5 is also divisible by 15--
+      if(i % 15 === 0) {
+      // If divisible by 15: Print FizzBuzz 
       console.log('FizzBuzz');
-    } else if (i % 3 ===0){
+      // Use else if conditional to determine if divisible by 3
+      } else if (i % 3 ===0){
       // If only divisible by 3: Print Fizz
       console.log('Fizz');
-    
+      // Use else if conditional to determine if divisible by 5
   } else if ( i % 5 === 0) {
-      // See if it is divisible by 5
-      // If so: Print Buzz
+      // If divisible by 5, Print Buzz
       console.log('Buzz');
+      // Use else conditional to see if the number is not divisible by 3 or 5
   } else {
-    // See if it is not divisible by 3 or 5
-    // If so: Print number
-    console.log(i);
-  
+      // If it is not, then Print number
+      console.log(i);
   }
 }
 
