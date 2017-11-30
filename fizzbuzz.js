1ab46586d7ebfa0adcfa278987861aeb01a531c5
Bill Vasilopoulos <bvasilop@gmail.com>
2017-11-29 18:53:29 -0800

    added case and switch version of fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -22,4 +22,26 @@
   }
 }
 
+//*****FizzBuzz using switch and case****//
+/*
+
+for (var x = 0; x <= 20; x++) {
+  switch (true) {
+    case (x % 15 === 0):
+        console.log("FizzBuzz");
+        break;
+    case (x % 3 === 0):
+        console.log("Fizz");
+        break;
+    case (x % 5 === 0):
+        console.log("Buzz");
+        break;
+    default:
+        console.log(x);
+        break;
+  }
+}
+
+*/
+
  
